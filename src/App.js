adacf9395c234e85ecca9888e0ff9f1b2a8ca9d6
muhammmed umer <www.muhammadumerpk@gmail.com>
2023-04-09 17:04:44 +0500

    5th one
    added the route import

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import GlobalStyle from "./globalStyles";
 import { ThemeProvider } from "styled-components";
-import{lightTheme,DarkTheme} from "./components/Themes";
+import { lightTheme, DarkTheme } from "./components/Themes";
+import { Switch } from "react-router-dom/cjs/react-router-dom";
+import { Route } from 'react-router-dom';
+import Main from "./components/Main";
+import MySkillsPage from "./components/MySkillsPage";
+import WorkPage from "./components/WorkPage";
+import BlogPage from "./components/BlogPage";
+import AboutPage from "./components/AboutPage";
+
 function App() {
   return (
     <>
       <GlobalStyle />
 
-<ThemeProvider theme={lightTheme}>
-
-</ThemeProvider>
+      <ThemeProvider theme={lightTheme}>
+        <Switch>
+          <Route exact path="/" components={Main}></Route>
+          <Route exact path="/about" components={AboutPage}></Route>
+          <Route exact path="/blog" components={BlogPage}></Route>
+          <Route exact path="/work" components={WorkPage}></Route>
+          <Route exact path="/skills" components={MySkillsPage}></Route>
+        </Switch>
+      </ThemeProvider>
     </>
   );
 }
