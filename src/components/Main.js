4c61e0eeaa443c5b4d8b3a1375e24f8e103004d9
muhammmed umer <www.muhammadumerpk@gmail.com>
2023-04-10 22:26:58 +0500

    5th one

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const MainContainer= styled.div`
+background: ${props => props.theme.text};
+width:100vw;
+height:100vh;
+`
+
+
 
 const Main = () => {
   return (
-    <div>
-      Main component/Page
-    </div>
+    <MainContainer>
+hhlll
+    </MainContainer>
   )
 }
 
